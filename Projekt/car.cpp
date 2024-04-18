305b8351d6eb5178c1c1c90997c5847a96d44e42
Damian <260353@student.pwr.edu.pl>
2024-04-18 04:20:35 +0200

    some changes

--- Projekt/car.cpp
+++ Projekt/car.cpp
@@ -1,2 +1,43 @@
 
 #include "car.h"
+#include <GL/glew.h>
+#include <pthread.h>
+
+//car is a rectangle that moves in a square path. It has a speed and a direction. It can be drawn using openGL. It is a thread.
+
+
+void draw_car(float corner1_x, float corner1_y, float corner2_x, float corner2_y){
+    glVertex2f(corner1_x, corner1_y);
+    glVertex2f(corner2_x, corner1_y);
+    glVertex2f(corner2_x, corner2_y);
+    glVertex2f(corner1_x, corner2_y);
+}
+
+void draw_all_cars(){
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_QUADS);
+    glColor3f(0.1f, 0.1f, 0.1);
+    float corner1_x = -0.1f;
+    float corner1_y = 0.1f;
+    float corner2_x = 0.1f;
+    float corner2_y = -0.1f;
+    draw_car(corner1_x, corner1_y, corner2_x, corner2_y);
+    glEnd();
+}
+
+void* car_thread(void* arg){
+    while(1){
+        draw_all_cars();
+    }
+    return NULL;
+}
+
+void start_car_thread(){
+    pthread_t car_thread_id;
+    pthread_create(&car_thread_id, NULL, car_thread, NULL);
+}
+
+void stop_car_thread(){
+    pthread_cancel(car_thread_id);
+}
+
--- Projekt/car.h
+++ Projekt/car.h
@@ -3,3 +3,16 @@
 #define SYSTEMY_OPERACYJNE_CAR_H
 
 #endif //SYSTEMY_OPERACYJNE_CAR_H
+
+
+void draw_car(float corner1_x, float corner1_y, float corner2_x, float corner2_y);
+
+void draw_all_cars();
+
+void* car_thread(void* arg);
+
+void start_car_thread();
+
+void stop_car_thread();
+
+
